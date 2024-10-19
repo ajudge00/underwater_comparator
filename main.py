b7e0344fc05a708dadcb721249d6b8d28f383184
Biro Armand <biro.armand@gmail.com>
2024-10-19 23:05:18 +0200

    image2 ids in code

--- main.py
+++ main.py
@@ -13,32 +13,33 @@ from sharpening import unsharp_mask
 
 
 class UI(QMainWindow):
-    def __init__(self):
+    def __init__(self, ui_path):
         super(UI, self).__init__()
 
-        uic.loadUi("image1.ui", self)
+        uic.loadUi(ui_path, self)
 
         # Widgets
-        self.img_left = self.findChild(QLabel, "label_image_original")
-        self.img_right = self.findChild(QLabel, "label_image_proc")
+        self.img_left = self.findChild(QLabel, "img_original")
+        self.img_right = self.findChild(QLabel, "img_result")
         self.button_load = self.findChild(QPushButton, "button_load")
         self.button_process = self.findChild(QPushButton, "button_process")
         self.button_save = self.findChild(QPushButton, "button_save")
-        self.combo_result = self.findChild(QComboBox, "combo_show_right")
+        self.combo_pipeline = self.findChild(QComboBox, "combo_pipeline")
+        self.combo_see_stage = self.findChild(QComboBox, "combo_see_stage")
 
-        self.slider_alpha_red = self.findChild(QSlider, "slider_alpha_red")
-        self.slider_alpha_blue = self.findChild(QSlider, "slider_alpha_blue")
+        self.slider_wb_alpha_red = self.findChild(QSlider, "slider_wb_alpha_red")
+        self.slider_wb_alpha_blue = self.findChild(QSlider, "slider_wb_alpha_blue")
         self.slider_gamma = self.findChild(QSlider, "slider_gamma")
-        self.slider_sigma = self.findChild(QSlider, "slider_sigma")
-        self.slider_strength = self.findChild(QSlider, "slider_strength")
+        self.slider_sharp_sigma = self.findChild(QSlider, "slider_sharp_sigma")
+        self.slider_sharp_strength = self.findChild(QSlider, "slider_sharp_strength")
 
-        self.label_alpha_red = self.findChild(QLabel, "label_alpha_red")
-        self.label_alpha_blue = self.findChild(QLabel, "label_alpha_blue")
-        self.label_gamma = self.findChild(QLabel, "label_gamma")
-        self.label_sigma = self.findChild(QLabel, "label_sigma")
-        self.label_strength = self.findChild(QLabel, "label_strength")
+        self.value_wb_alpha_red = self.findChild(QLabel, "value_wb_alpha_red")
+        self.value_wb_alpha_blue = self.findChild(QLabel, "value_wb_alpha_blue")
+        self.value_gamma = self.findChild(QLabel, "value_gamma")
+        self.value_sharp_sigma = self.findChild(QLabel, "value_sharp_sigma")
+        self.value_sharp_strength = self.findChild(QLabel, "value_sharp_strength")
 
-        self.check_wb_precomp = self.findChild(QCheckBox, "check_wbpc")
+        self.check_wb_precomp = self.findChild(QCheckBox, "check_wb_pc")
         self.check_wb = self.findChild(QCheckBox, "check_wb")
         self.check_gamma_sharp_msf = self.findChild(QCheckBox, "check_gamma_sharp_msf")
 
@@ -52,20 +53,20 @@ class UI(QMainWindow):
         self.image_sharp = None
         self.image_sharp_gamma = None
 
-        self.alpha_red = self.slider_alpha_red.value() / 10.0
-        self.alpha_blue = self.slider_alpha_blue.value() / 10.0
+        self.alpha_red = self.slider_wb_alpha_red.value() / 10.0
+        self.alpha_blue = self.slider_wb_alpha_blue.value() / 10.0
         self.gamma = self.slider_gamma.value() / 10.0
-        self.sigma = self.slider_sigma.value()
-        self.strength = self.slider_strength.value() / 10.0
+        self.sigma = self.slider_sharp_sigma.value()
+        self.strength = self.slider_sharp_strength.value() / 100.0
 
         # Connects
         self.button_load.clicked.connect(self.load_image)
         self.button_process.clicked.connect(self.process_image)
-        self.slider_alpha_red.valueChanged.connect(self.set_alpha_red)
-        self.slider_alpha_blue.valueChanged.connect(self.set_alpha_blue)
+        self.slider_wb_alpha_red.valueChanged.connect(self.set_alpha_red)
+        self.slider_wb_alpha_blue.valueChanged.connect(self.set_alpha_blue)
         self.slider_gamma.valueChanged.connect(self.set_gamma)
-        self.slider_sigma.valueChanged.connect(self.set_sigma)
-        self.slider_strength.valueChanged.connect(self.set_strength)
+        self.slider_sharp_sigma.valueChanged.connect(self.set_sigma)
+        self.slider_sharp_strength.valueChanged.connect(self.set_strength)
 
         self.check_wb_precomp.stateChanged.connect(
             lambda: self.change_result_combo("wbpc", self.check_wb_precomp.isChecked())
@@ -147,34 +148,34 @@ class UI(QMainWindow):
         # Property
         self.alpha_red = value / 10.0
         # Label
-        new_value = str(self.slider_alpha_red.value() / 10.0)
-        self.label_alpha_red.setText(new_value)
+        new_value = str(self.slider_wb_alpha_red.value() / 10.0)
+        self.value_wb_alpha_red.setText(new_value)
 
     def set_alpha_blue(self, value):
         # Property
         self.alpha_blue = value / 10.0
         # Label
-        new_value = str(self.slider_alpha_blue.value() / 10.0)
-        self.label_alpha_blue.setText(new_value)
+        new_value = str(self.slider_wb_alpha_blue.value() / 10.0)
+        self.value_wb_alpha_blue.setText(new_value)
 
     def set_alpha_blue_spinner(self, value):
         self.alpha_blue = value
-        self.slider_alpha_blue.setValue(int(self.alpha_blue * 10))
+        self.slider_wb_alpha_blue.setValue(int(self.alpha_blue * 10))
 
     def set_gamma(self, value):
         self.gamma = value / 10.0
         new_value = str(self.slider_gamma.value() / 10.0)
-        self.label_gamma.setText(new_value)
+        self.value_gamma.setText(new_value)
 
     def set_sigma(self, value):
         self.sigma = value
-        new_value = str(self.slider_sigma.value())
-        self.label_sigma.setText(new_value)
+        new_value = str(self.slider_sharp_sigma.value())
+        self.value_sharp_sigma.setText(new_value)
 
     def set_strength(self, value):
         self.strength = value / 10.0
-        new_value = str(self.slider_strength.value() / 10.0)
-        self.label_strength.setText(new_value)
+        new_value = str(self.slider_sharp_strength.value() / 10.0)
+        self.value_sharp_strength.setText(new_value)
 
     def change_result_combo(self, choice: str, checked: bool):
         if checked:
@@ -186,11 +187,11 @@ class UI(QMainWindow):
         self.build_result_combo()
 
     def build_result_combo(self):
-        self.combo_result.clear()
+        self.combo_see_stage.clear()
         for group in self.current_choice_groups:
-            self.combo_result.addItems(self.view_choices[group])
+            self.combo_see_stage.addItems(self.view_choices[group])
 
-        self.combo_result.setCurrentIndex(self.combo_result.count() - 1)
+        self.combo_see_stage.setCurrentIndex(self.combo_see_stage.count() - 1)
 
     def make_display_img(self, img, side):
         if img.dtype == np.float32:
@@ -208,5 +209,5 @@ class UI(QMainWindow):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    UIWindow = UI()
+    UIWindow = UI("image2.ui")
     app.exec_()
